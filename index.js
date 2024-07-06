67c837d64c0a341b9f18022cae392c958ada2150
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2024-07-06 12:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('SHRUTI'); //write a response to the client
+  res.write('Hello everyone'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
